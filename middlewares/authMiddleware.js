d874d85b85464d107747d32bb5c3ba366af2e824
Adham Elganzoury <adham54732@gmail.com>
2025-07-18 01:49:34 +0300

    add audio upload future

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,25 +1,23 @@
- const jwt = require('jsonwebtoken')
- const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET
- 
- function authMiddleware(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    if (!authHeader) {
-        return res.status(401).json({ message: 'Missing Authorization header' });
-    }
+const jwt = require("jsonwebtoken");
+const ACCESS_TOKEN_SECRET = process.env.JWT_SECRET_ACCESS;
 
-    
-    const [scheme, token] = authHeader.split(' ');
-    if (scheme !== 'Bearer' || !token) {
-        return res.status(401).json({ message: 'Invalid Authorization format' });
-    }
-    try {
-        
-        const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        return res.status(401).json({ message: 'Invalid or expired token' });
-    }
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    return res.status(401).json({ message: "Missing Authorization header" });
+  }
+
+  const [scheme, token] = authHeader.split(" ");
+  if (scheme !== "Bearer" || !token) {
+    return res.status(401).json({ message: "Invalid Authorization format" });
+  }
+  try {
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
 }
 
-module.exports = authMiddleware
+module.exports = authMiddleware;
