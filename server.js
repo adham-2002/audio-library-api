d566f8bf8109618b4e47b6854b53c5f4173ce3f1
Adham Elganzoury <adham54732@gmail.com>
2025-07-17 20:47:16 +0300

    add user registration,login and get profile

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const app = require("./app");
-const chalk = require("chalk");
-const PORT = process.env.PORT || 3000;
+const connectDB = require("./config/db");
+
+connectDB();
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(chalk.yellow("================================="));
-  console.log(chalk.green.bold("✅ Server is up and running!"));
-  console.log(chalk.blue(`🌐 URL: http://localhost:${PORT}`));
-  console.log(chalk.yellow("================================="));
-  const env = process.env.NODE_ENV || "development";
-  console.log(chalk.magenta(`🛠️  Running in ${env} mode`));
+  console.log(`Server Running on http://localhost:${PORT}`);
 });
