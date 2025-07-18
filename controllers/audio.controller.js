fdf4eeee76a04d059a86d93fa018981c46c92d1f
Adham Elganzoury <adham54732@gmail.com>
2025-07-18 15:13:48 +0300

    refactor multer middleware

--- controllers/audio.controller.js
+++ controllers/audio.controller.js
@@ -4,15 +4,26 @@
 // DELETE /api/audio/:id — delete your own audio file
 
 const Audio = require("../models/audio.model");
+const fs = require("fs").promises;
+// async function deleteFile(path) {
+//   try {
+//     await fs.unlink(path);
+//   } catch (err) {
+//     console.error("Error deleting file:", err);
+//   }
+// }
 const uploadAudio = async (req, res, next) => {
   const { title, genre, isPublic } = req.body;
   const userId = req.user.id;
   // save audio in the database
+  console.log(req.files.cover?.[0].filename);
   const audioData = {
     title,
     genre,
     privacy: isPublic ? "public" : "private",
     user: userId,
+    coverName: req.files.cover?.[0].filename,
+    audioName: req.files.audio?.[0].filename,
   };
   try {
     const newAudio = await Audio.create(audioData);
@@ -21,6 +32,7 @@ const uploadAudio = async (req, res, next) => {
     next(error);
   }
 };
+
 const getPublicAudios = async (req, res, next) => {
   const publicAudios = await Audio.find({ privacy: "public" });
   res.status(200).json({
@@ -39,9 +51,28 @@ const getUserAudios = async (req, res, next) => {
     myAudios,
   });
 };
-
+const deleteAudio = async (req, res, next) => {
+  //name of file is user_${req.user.id}. but how i can get extention of file?
+  const path = `uploads/audios/user_${req.user.id}`;
+  const userId = req.user.id;
+  const audioId = req.params.id;
+  console.log(userId);
+  console.log(audioId);
+  try {
+    const something = await Audio.deleteOne({ user: userId, _id: audioId });
+    console.log(something);
+  } catch (err) {
+    next(
+      new Error(
+        "AUdio not exists or you not the owner of this audio to delete it"
+      )
+    );
+  }
+  res.status(204).end();
+};
 module.exports = {
   uploadAudio,
   getPublicAudios,
   getUserAudios,
+  deleteAudio,
 };
