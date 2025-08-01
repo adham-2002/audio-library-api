4c92c8901b4bec2b0146461f547909b494a0cfee
Adham Elganzoury <adham54732@gmail.com>
2025-08-01 21:42:24 +0300

    implement apifeatures

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -3,16 +3,33 @@ const path = require("path");
 const fs = require("fs").promises;
 const apiError = require("../utils/apiError");
 const asyncErrorHandler = require("../utils/asyncErrorHandler");
+const ApiFeatures = require("../utils/apiFeatures");
+
 const getAllAudios = asyncErrorHandler(async (req, res, next) => {
-  const audios = (await Audio.find()).flat();
+  // Count total documents for pagination
+  const documentsCount = await Audio.countDocuments();
+
+  // Apply ApiFeatures
+  const apiFeatures = new ApiFeatures(Audio.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .search("Audio")
+    .paginate(documentsCount);
+
+  const audios = await apiFeatures.mongooseQuery;
+
   res.status(200).json({
     status: "success",
+    message: "All audios fetched successfully",
     results: audios.length,
+    pagination: apiFeatures.paginationResult,
     data: {
       audios,
     },
   });
 });
+
 module.exports = {
   getAllAudios,
 };
